673fa44c9f0618fc8281f26afa73d64805f9a12d
SomeshwarJ <senthilsomeshwar2002@gmail.com>
2023-11-22 22:02:47 +0530

    first commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-	const database = process.env.MONGO_URI;
+	const database = "mongodb+srv://senthilsomeshwar2002:mYKWQktfHX407kYT@cluster0.nwvdtsx.mongodb.net/?retryWrites=true&w=majority";
 	mongoose.set("strictQuery", true);
 	mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
     	.then(() => console.log('Database Connected...'))
